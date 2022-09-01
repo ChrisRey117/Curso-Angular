015e105fe634b2e524c6d9bdd8e99c303c05a5f9
Reyes Hernandez <c.reyes.hernandez@accenture.com>
2022-09-01 12:46:26 -0500

    Video 7

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -7,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmpleadoComponent implements OnInit {
 
+  nombre = "Christian Manuel";
+  apellido = "Reyes Hernandez";
+  private edad = 22;
+  hobbit = "Caminar";
+
+  getEdad(){
+    return this.edad;
+  }
   constructor() { }
 
   ngOnInit(): void {
